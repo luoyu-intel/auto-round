c6c374758d8b2fe507dff2a9693144cb6981a057
Luo, Yu <yu.luo@intel.com>
2026-04-16 09:02:18 +0000

    add custom asym search

--- auto_round/data_type/int.py
+++ auto_round/data_type/int.py
@@ -139,6 +139,98 @@ def search_scales_zp(
 
     return scale, zp
 
+def t_DQ(Q):
+    if len(Q) == 3:
+        return (Q[0] - Q[2]) * Q[1]
+    return Q[0] * Q[1]
+
+
+def t_dequant_weight(Q_main, Q_res, shape, n_iter=100):
+    dq = t_DQ(Q_main).reshape(shape)
+    n = min(len(Q_res), n_iter)
+    for i in range(n):
+        dq += t_DQ(Q_res[i]).reshape(shape)
+    return dq
+
+def t_dyn_quant(arr, bits=4, dir=-1, asym=True, iter=2, qw=None):
+    a_min = arr.min(dir, keepdim=True)[0]
+    a_min = torch.clamp(a_min, max=0)
+    a_max = arr.max(dir, keepdim=True)[0]
+    a_max = torch.clamp(a_max, min=0)
+    a_min_abs = torch.abs(a_min)
+    a_max_abs = torch.abs(a_max)
+    a_absmax = torch.max(a_min_abs, a_max_abs)
+    Q = (1 << bits) - 1
+    FullQ = 1 << (bits - 1)
+    denorm = a_max - a_min
+
+    def asym_quant_iter(_Q):
+        scale0 = _Q / denorm
+        scale0[denorm.abs() <= 1e-4] = 1
+        zero_point0 = torch.round(-a_min * scale0)
+        zero_point0 = torch.clamp(zero_point0, 0, Q) - FullQ
+        qarr0 = torch.clamp(torch.round(arr * scale0 + zero_point0), 0, Q) - FullQ
+        scale0 = 1 / scale0
+        dq0 = t_dequant_weight([qarr0, scale0, zero_point0], [], arr.shape)
+        err0 = abs(arr - dq0)
+        err0 = torch.sum(err0, dim=dir, keepdim=True)
+        if qw is not None:
+            err0.mul_(qw)
+        return err0, qarr0, scale0, zero_point0
+
+    if asym:
+        StartQ = Q
+        err, qarr, scale, zero_point = asym_quant_iter(StartQ)
+        if iter > 1:
+            delta = 4 / (iter - 1)
+            StartQ = Q - 0.5
+            for i in range(iter - 1):
+                _ret = asym_quant_iter(StartQ)
+                qarr = torch.where(_ret[0] < err, _ret[1], qarr)
+                scale = torch.where(_ret[0] < err, _ret[2], scale)
+                zero_point = torch.where(_ret[0] < err, _ret[3], zero_point)
+                err = torch.where(_ret[0] < err, _ret[0], err)
+                StartQ += delta
+        qarr_a = qarr
+        scale_a = scale
+        zero_point_a = zero_point
+        err_a = err
+
+    def full_quant_iter(_Q):
+        # fullrange
+        max_v = (2 * (a_min_abs > a_max_abs).int() - 1) * a_absmax
+        scale1 = max_v / _Q
+        scale1[scale1 == 0] = 1
+        qarr1 = arr / scale1
+        qarr1 = torch.round(qarr1)
+        qarr1 = torch.clamp(qarr1, -FullQ, FullQ - 1)
+        dq1 = t_dequant_weight([qarr1, scale1], [], arr.shape)
+        err1 = abs(arr - dq1)
+        err1 = torch.sum(err1, dim=dir, keepdim=True)
+        if qw is not None:
+            err1.mul_(qw)
+        return err1, qarr1, scale1
+
+    StartQ = FullQ
+    err, qarr, scale = full_quant_iter(StartQ)
+    if iter > 1:
+        delta = 4 / (iter - 1)
+        StartQ = FullQ - 1
+        for i in range(iter - 1):
+            _ret = full_quant_iter(StartQ)
+            qarr = torch.where(_ret[0] < err, _ret[1], qarr)
+            scale = torch.where(_ret[0] < err, _ret[2], scale)
+            err = torch.where(_ret[0] < err, _ret[0], err)
+            StartQ += delta
+    if asym:
+        zero_point = torch.zeros_like(zero_point_a)
+        qarr = torch.where(err_a < err, qarr_a, qarr)
+        scale = torch.where(err_a < err, scale_a, scale)
+        zero_point = torch.where(err_a < err, zero_point_a, zero_point)
+        return qarr, scale, zero_point
+    else:
+        return qarr, scale
+
 
 @register_dtype("rtn_int_sym")
 def quant_tensor_rtn_sym(tensor, bits=4, group_size=-1, v=0, q_scale_thresh=1e-5, imatrix=None, **kwargs):
@@ -203,11 +295,13 @@ def quant_tensor_rtn_asym(tensor, bits=4, group_size=-1, v=0, q_scale_thresh=1e-
         imatrix = imatrix.reshape(tensor.shape)
 
         imatrix = _imatrix_handle_zero(imatrix, tensor, bits)
-
-    scale, zp = search_scales_zp(tensor, bits, qw=imatrix)
-    scale = torch.where(scale < 0, torch.clamp(scale, max=-q_scale_thresh), torch.clamp(scale, min=q_scale_thresh))
-    int_w = round_ste(tensor / scale + v)
-    q = torch.clamp(int_w + zp, 0, maxq)
+    if True:
+        q, scale, zp =t_dyn_quant(tensor, bits=bits, dir=-1, asym=True, iter=20, qw=imatrix)
+    else:
+        scale, zp = search_scales_zp(tensor, bits, qw=imatrix)
+        scale = torch.where(scale < 0, torch.clamp(scale, max=-q_scale_thresh), torch.clamp(scale, min=q_scale_thresh))
+        q = torch.round(tensor / scale  + zp)
+        q = torch.clamp(q, 0, maxq)
     qdq_result = (scale * (q - zp)).to(tensor.dtype)
     qdq_result = revert_tensor_by_pad(qdq_result, orig_shape=orig_shape, pad_len=pad_len)
     return qdq_result, scale, zp
