34d673030ee5399070f57eed4aef5564e86c572a
Luo, Yu <yu.luo@intel.com>
2026-04-21 08:47:07 +0000

    opt quant speed

--- run_rrq_22_asym.sh
+++ run_rrq_22_asym.sh
@@ -28,20 +28,15 @@ OUTPUT_R0_OUTPUT_MERGE_DIR="${OUTPUT_ROOT%/}/${MODEL_NAME}-${SCHEME}-R0-${SCHEME
 
 export AUTO_ROUND_IMATRIX_FILE="${OUTPUT_ROOT%/}/imatrix.pt"
 # save the intermediate imatrix for auto-rounding
-python -m auto_round --model "$MODEL_PATH" --scheme "$SCHEME"  --iters 0 --output_dir "$OUTPUT_DIR"
+python -m auto_round --model "$MODEL_PATH" --scheme "$SCHEME"  --format "fake" --iters 0 --output_dir "$OUTPUT_DIR" --asym
 unset AUTO_ROUND_IMATRIX_FILE
 
 BASE_OUTPUT_DIR="${OUTPUT_DIR}/${MODEL_NAME}-${SUFFIX}"
-rm -rf "$BASE_OUTPUT_DIR"
-
-
-auto-round-best --enable_alg_ext --lr 2e-3 --model "$MODEL_PATH" --scheme "$SCHEME" --format "fake" --output_dir "$OUTPUT_DIR" --low_gpu_mem_usage
-	
 
 python get_residual.py -i "$MODEL_PATH" -q "$BASE_OUTPUT_DIR" -o "${OUTPUT_R0_DIR}" --operation sub --device cpu
 
 export AUTO_ROUND_LOAD_IMATRIX_FILE="${OUTPUT_ROOT%/}/imatrix.pt"
-python -m auto_round --model "$OUTPUT_R0_DIR" --scheme "$SCHEME" --format "fake" --iters 0 --output_dir "$OUTPUT_R0_OUTPUT_DIR" --asym --low_gpu_mem_usage
+python -m auto_round --model "$OUTPUT_R0_DIR" --scheme "$SCHEME" --format "fake" --iters 0 --output_dir "$OUTPUT_R0_OUTPUT_DIR" --asym
 unset AUTO_ROUND_LOAD_IMATRIX_FILE
 R0_OUTPUT_DIR="${OUTPUT_R0_OUTPUT_DIR}/${MODEL_NAME}-${SCHEME}-R0-${SUFFIX}"
 
